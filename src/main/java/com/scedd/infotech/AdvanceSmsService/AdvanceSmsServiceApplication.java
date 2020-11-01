693af92a31b2e0ce43419c1a4128128afcb98e3b
Kpadhi <Evoke@0921013053>
2020-11-01 21:52:34 +0530

    first commit

---
+++ src/main/java/com/scedd/infotech/AdvanceSmsService/AdvanceSmsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.scedd.infotech.AdvanceSmsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvanceSmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvanceSmsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scedd/infotech/AdvanceSmsService/AdvanceSmsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scedd.infotech.AdvanceSmsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvanceSmsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
