075e7b65de0bebcfdc1bfabb08d8c6d23c1f0a45
Kpadhi <coderkali@gmail.com>
2020-11-04 08:53:17 +0530

    swagger added

--- src/main/java/com/sceddinfo/sms/rest/controller/WelcomeController.java
+++ src/main/java/com/sceddinfo/sms/rest/controller/WelcomeController.java
@@ -1,14 +1,26 @@
 package com.sceddinfo.sms.rest.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
+@Api(value = "user", description = "Rest API for Advance Sms Servie", tags = "sunil api")
 public class WelcomeController {
-	
-	@RequestMapping(value="/test")
-	public String testSpringAOP() {
-		return "test";
+
+	@RequestMapping(value = "/hello/{name}")
+	@ApiOperation(value = "Display Welcome message ", response = Greeting.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "OK"),
+			@ApiResponse(code = 404, message = "The resource not found") })
+	public Greeting message(@PathVariable("name") String name) {
+		Greeting msg = new Greeting(name, "Hello..... " + name);
+		return msg;
 	}
 
 }
--- src/main/java/com/sceddinfo/sms/rest/util/ModelMapperImpl.java
+++ src/main/java/com/sceddinfo/sms/rest/util/ModelMapperImpl.java
@@ -4,12 +4,16 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("restModelMapperImp")
 public class ModelMapperImpl {
 
 	@Autowired
 	ModelMapper modelMapper;
+
+
 	public <T> Object convert(Object source,Class<T> clazz) {
 		return modelMapper.map(source, clazz);
 	}
+
+
 }
